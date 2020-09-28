fe703a1327eb2cff3fc909f2ebeeedf3c9ee6d62
Vinod Pillai <vinodthebest@gmail.com>
2020-09-28 23:51:45 +0530

    saga-customer: Added dependency and create project
    - Added dependency and gitignore file.

---
+++ src/main/java/com/vinod/saga/choreography/customer/SagaChoreographyCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vinod.saga.choreography.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaChoreographyCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaChoreographyCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinod/saga/choreography/customer/SagaChoreographyCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinod.saga.choreography.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaChoreographyCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
